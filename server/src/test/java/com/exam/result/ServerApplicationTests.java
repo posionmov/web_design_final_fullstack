4b19515e29a3e4599270c92ab0bd67d3bbca2110
Sergei Galanov <sergei.galanov@peridotfinancing.com>
2023-10-10 20:50:53 +0200

    minor fix

--- server/src/test/java/com/exam/result/ServerApplicationTests.java
+++ server/src/test/java/com/exam/result/ServerApplicationTests.java
@@ -1,13 +1,10 @@
 package com.exam.result;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
